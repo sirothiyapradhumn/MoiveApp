04fd117f9748e59075167a962e5377991a8d23f7
Pradhumn Siorthiya <sirothiyapradhumn@gmail.com>
2022-07-09 22:09:05 +0530

    List mai hover with state and pagination added

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react'
 import { movies } from './getMovies'
 export default class List extends Component {
+  constructor(){
+    super();
+    this.state = {
+      hover: "",
+    };
+  }
+
+  handleEnter = (id) =>{
+    this.setState({
+      hover:id
+    })
+  };
+
+  handleLeave = () => {
+    this.setState({
+      hover:''
+    })
+  }
   render() {
     let movie = movies.results;
     return (
@@ -18,17 +36,30 @@ export default class List extends Component {
               <div className='movie-list'>
                 {
                   movie.map((movieObj) =>(
-                    <div className="card movie-card">
-                      <img src={`https://image.tmdb.org/t/p/original${movieObj.backdrop_path}`} className="card-img-top banner-image" alt="..."  style={{height:"40vh", width:"20vw"}}/>
+                    <div className="card movie-card" onMouseEnter={()=>{this.handleEnter(movieObj.id)}} onMouseLeave={this.handleLeave}>
+                      <img src={`https://image.tmdb.org/t/p/original${movieObj.backdrop_path}`} className="card-img-top movie-image" alt="..."  style={{height:"40vh"}}/>
                       <h5 className="card-title movie-title">{movieObj.original_title}</h5>
                       <div className='button-wrapper'>
-                        <a href="#" class="btn btn-primary movie-button">Add to Favourites</a>
+                        {
+                          this.state.hover == movieObj.id && <a href="#" class="btn btn-primary movie-button">Add to Favourites</a>
+                        }
+                        
                       </div>
                     </div>
                   ))
                 }
               </div>
-              
+              <div className='pagination'>
+                <nav aria-label="Page navigation example">
+                  <ul class="pagination">
+                    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                  </ul>
+                </nav>
+              </div>
             </div>
 
           )
