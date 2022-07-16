75b41f74282c3144e98a7218da05ceb57052e305
Pradhumn Siorthiya <sirothiyapradhumn@gmail.com>
2022-07-16 12:04:27 +0530

    :fire: axios, api fetch, lifecycle methods

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
-import { movies } from './getMovies'
+//import { movies } from './getMovies'
+import axios from 'axios'
+import API_KEY from '../secrets';
 export default class List extends Component {
   constructor(){
     super();
+    //console.log("constructor is called");
     this.state = {
       hover: "",
+      parr: [1], //ab tak mai konse page par  hu, or what page am i showing
+      currentPage: 1,
+      movies: [],
     };
   }
 
@@ -19,12 +25,25 @@ export default class List extends Component {
       hover:''
     })
   }
+
+  async componentDidMount(){
+    //console.log("componentDidMount is called");
+    //console.log(API_KEY);
+    let ans = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=${this.state.currentPage}`);
+    console.log(ans.data);
+
+    this.setState({
+      movies:[...ans.data.results] //spread
+    })
+  } 
+
   render() {
-    let movie = movies.results;
+    //console.log("render is called");
+    //let movies = movies.results;
     return (
       <>
         {
-          movie.length == 0 ? (
+          this.state.movies.length == 0 ? (
             <div className="spinner-grow text-dark" role="status">
               <span className="sr-only">Loading...</span>
             </div>
@@ -35,7 +54,7 @@ export default class List extends Component {
               </h3>
               <div className='movie-list'>
                 {
-                  movie.map((movieObj) =>(
+                  this.state.movies.map((movieObj) =>(
                     <div className="card movie-card" onMouseEnter={()=>{this.handleEnter(movieObj.id)}} onMouseLeave={this.handleLeave}>
                       <img src={`https://image.tmdb.org/t/p/original${movieObj.backdrop_path}`} className="card-img-top movie-image" alt="..."  style={{height:"40vh"}}/>
                       <h5 className="card-title movie-title">{movieObj.original_title}</h5>
@@ -53,9 +72,11 @@ export default class List extends Component {
                 <nav aria-label="Page navigation example">
                   <ul class="pagination">
                     <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                    <li class="page-item"><a class="page-link" href="#">1</a></li>
-                    <li class="page-item"><a class="page-link" href="#">2</a></li>
-                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    {
+                      this.state.parr.map((pageNum)=>(
+                        <li class="page-item"><a class="page-link" href="#">{pageNum}</a></li>
+                      ))
+                    }
                     <li class="page-item"><a class="page-link" href="#">Next</a></li>
                   </ul>
                 </nav>
